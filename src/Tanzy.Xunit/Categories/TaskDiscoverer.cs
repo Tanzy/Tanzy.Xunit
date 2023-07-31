74b19d636c880573363b2d710422917cf7b19d31
Ken Lea <git@kenlea.dev>
2023-07-31 09:37:43 +0100

    Add a Task category

--- src/Tanzy.Xunit/Categories/TaskDiscoverer.cs
+++ src/Tanzy.Xunit/Categories/TaskDiscoverer.cs
@@ -6,29 +6,29 @@ namespace Tanzy.Xunit.Categories
 {
     // <summary>
     /// This class discovers all of the tests and test classes that have
-    /// applied the UserStory attribute
+    /// applied the Task attribute
     /// </summary>
-    public class UserStoryDiscoverer : ITraitDiscoverer
+    public class TaskDiscoverer : ITraitDiscoverer
     {
         /// <summary>
         /// The fully qualified name of this class
         /// </summary>
-        internal const string FullyQualifiedName = NamespaceUtil.Namespace + "." + nameof(Categories) + "." + nameof(UserStoryDiscoverer);
+        internal const string FullyQualifiedName = NamespaceUtil.Namespace + "." + nameof(Categories) + "." + nameof(TaskDiscoverer);
 
         /// <summary>
-        /// Gets the trait values from the UserStory attribute.
+        /// Gets the trait values from the Task attribute.
         /// </summary>
         /// <param name="traitAttribute">The trait attribute containing the trait values.</param>
         /// <returns>The trait values.</returns>
         public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
         {
-            var storyId = traitAttribute.GetNamedArgument<string>("UserStoryId");
+            var taskId = traitAttribute.GetNamedArgument<string>("TaskId");
 
-            yield return new KeyValuePair<string, string>("Category", "UserStory");
+            yield return new KeyValuePair<string, string>("Category", "Task");
             
-            if (!string.IsNullOrWhiteSpace(storyId))
+            if (!string.IsNullOrWhiteSpace(taskId))
             {
-                yield return new KeyValuePair<string, string>("UserStory", storyId);
+                yield return new KeyValuePair<string, string>("Task", taskId);
             }
         }
     }
